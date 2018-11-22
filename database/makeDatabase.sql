7e437b908006b4e6c0af9aacbf36c2cf68192fb7
Eura Shin <eesh227@uky.edu>
2018-11-22 00:07:55 -0500

    added fake data

--- database/makeDatabase.sql
+++ database/makeDatabase.sql
@@ -12,6 +12,9 @@ CREATE TABLE customer
 	name VARCHAR(20),
 	password VARCHAR(20) NOT NULL
 	);
+LOAD DATA LOCAL INFILE './customers.txt'
+INTO TABLE customer
+FIELDS TERMINATED BY ',';
 
 CREATE TABLE restaurant 
 	(
@@ -33,28 +36,37 @@ FIELDS TERMINATED BY ',';
 CREATE TABLE menuitem
 	(
 	rid INT NOT NULL, 
-	name VARCHAR(20) NOT NULL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
 	price FLOAT, 
+    PRIMARY KEY (rid, name),
 	FOREIGN KEY (rid) REFERENCES restaurant(rid)
 	);
+LOAD DATA LOCAL INFILE './menuitems.txt'
+INTO TABLE menuitem
+FIELDS TERMINATED BY ',';
 
 CREATE TABLE ingredient
 (
-	name VARCHAR(20) NOT NULL,
-	foodGroup VARCHAR(20) NOT NULL,
-    PRIMARY KEY (name, foodGroup)  
+    name VARCHAR(20) NOT NULL,
+    foodGroup VARCHAR(20) NOT NULL, 
+    PRIMARY KEY (name, foodGroup)
 );
+LOAD DATA LOCAL INFILE './ingredients.txt'
+INTO TABLE ingredient
+FIELDS TERMINATED BY ',';
 
 CREATE TABLE contains
 (
-	itemName VARCHAR(20) NOT NULL, 
-	rid INT NOT NULL, 
-	ingredientName VARCHAR(20) NOT NULL, 
-	PRIMARY KEY (itemName, ingredientName, rid), 
-	FOREIGN KEY (itemName) REFERENCES menuitem(name),
-	FOREIGN KEY (rid) REFERENCES restaurant(rid),  
-	FOREIGN KEY (ingredientName) REFERENCES ingredient(name) 
+    name VARCHAR(20) NOT NULL, 
+    rid INT NOT NULL, 
+    ingredientName VARCHAR(20) NOT NULL, 
+    PRIMARY KEY (name, ingredientName, rid), 
+    FOREIGN KEY (rid) REFERENCES restaurant(rid),  
+    FOREIGN KEY (ingredientName) REFERENCES ingredient(name) 
 );
+LOAD DATA LOCAL INFILE './contains.txt'
+INTO TABLE contains
+FIELDS TERMINATED BY ',';
 
 CREATE TABLE reviews
 (
@@ -67,24 +79,33 @@ CREATE TABLE reviews
 	rating INT NOT NULL, 
 	review VARCHAR(250)
 );
+LOAD DATA LOCAL INFILE './reviews.txt'
+INTO TABLE reviews
+FIELDS TERMINATED BY ',';
 
 CREATE TABLE favorites
 (
-	itemName VARCHAR(20) NOT NULL, 
-	rid INT NOT NULL, 
-	PRIMARY KEY (itemName, rid), 
-	FOREIGN KEY (itemName) REFERENCES menuitem(name), 
-	FOREIGN KEY (rid) REFERENCES restaurant(rid)
+    username VARCHAR(20) NOT NULL,
+    itemName VARCHAR(20) NOT NULL, 
+    rid INT NOT NULL, 
+    PRIMARY KEY (username, rid, itemName), 
+    FOREIGN KEY (rid) REFERENCES restaurant(rid), 
+    FOREIGN KEY (username) REFERENCES customer(username)
 );
+LOAD DATA LOCAL INFILE './favorites.txt'
+INTO TABLE favorites
+FIELDS TERMINATED BY ',';
 
 
 CREATE TABLE restrictions 
 (
-	username VARCHAR(20) NOT NULL, 
-	ingredientName VARCHAR(20) NOT NULL, 
-    foodGroup VARCHAR(20) NOT NULL,
-	PRIMARY KEY (username, ingredientName, foodGroup), 
-	FOREIGN KEY (username) REFERENCES customer(username), 
-	FOREIGN KEY (ingredientName) REFERENCES ingredient(name), 
-    FOREIGN KEY (foodGroup) REFERENCES ingredient(foodGroup)
+    username VARCHAR(20) NOT NULL, 
+    ingredientName VARCHAR(20) NOT NULL, 
+    foodGroup VARCHAR(20) NOT NULL, 
+    PRIMARY KEY (username, ingredientName, foodGroup), 
+    FOREIGN KEY (username) REFERENCES customer(username), 
+    FOREIGN KEY (ingredientName) REFERENCES ingredient(name)
 );
+LOAD DATA LOCAL INFILE './restrictions.txt'
+INTO TABLE restrictions
+FIELDS TERMINATED BY ',';
